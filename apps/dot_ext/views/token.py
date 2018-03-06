49dd496b90b5298e00c23260b49a44ad72f7b01c
David Gage <whytheplatypus@gmail.com>
2018-03-06 08:25:36 -0500

    Add interface to manage a user's tokens A properly scoped application should be able to help a user manage other application's access.

---
+++ apps/dot_ext/views/token.py
@@ -0,0 +1,35 @@
+# TODO: Lock down endpoint via a scope
+from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework import serializers
+from oauth2_provider.models import AccessToken
+from oauth2_provider.ext.rest_framework import OAuth2Authentication
+from ..models import Application
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ('id', 'name', 'logo_uri', 'tos_uri', 'policy_uri', 'contacts')
+
+
+class AccessTokenSerializer(serializers.ModelSerializer):
+    application = ApplicationSerializer(read_only=True)
+
+    class Meta:
+        model = AccessToken
+        fields = '__all__'
+
+
+# TODO limit with mixins
+class AuthorizedTokens(viewsets.GenericViewSet,
+                       mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.DestroyModelMixin):
+
+    authentication_classes = [OAuth2Authentication]
+    serializer_class = AccessTokenSerializer
+
+    def get_queryset(self):
+        return AccessToken.objects.select_related("application").filter(user=self.request.user)
+
