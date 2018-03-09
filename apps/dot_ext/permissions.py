00af373db31b67716b8de016d1f26bb9416652d5
David Gage <whytheplatypus@gmail.com>
2018-03-09 11:29:36 -0500

    Regex matching for protected resources path - Allow view to set minimum required scopes - Throw error if neither view nor protected capabilities define at least one required scope

--- apps/dot_ext/permissions.py
+++ apps/dot_ext/permissions.py
@@ -1,28 +1,43 @@
 import json
+import re
+from django.core.exceptions import ImproperlyConfigured
 from oauth2_provider.contrib.rest_framework import permissions
 from apps.capabilities.models import ProtectedCapability
 
 
-def path_match(route, path):
+def path_match(pattern, path):
     # TODO allow pattern matching
+
     # or something?
-    return route == path
+    return re.match(pattern, path)
+
+
+def build_required_scopes(method, path):
+    required_scopes = []
+    possible_scopes = ProtectedCapability.objects.all()
+    for possible_scope in possible_scopes:
+        routes = json.loads(possible_scope.protected_resources)
+        for route in routes:
+            if route[0] == method:
+                if path_match(route[1], path):
+                    required_scopes.append(possible_scope.slug)
+    return required_scopes
 
 
 class TokenHasRouteScope(permissions.TokenHasScope):
 
     def get_scopes(self, request, view):
-        # get route from request
-        path = request.path
-        method = request.method
-        required_scopes = []
-        # TODO build scope list by finding the scopes with this in the list
-        possible_scopes = ProtectedCapability.objects.all()
-        for possible_scope in possible_scopes:
-            routes = json.loads(possible_scope.protected_resources)
-            for route in routes:
-                if route[0] == method:
-                    if path_match(route[1], path):
-                        required_scopes.append(possible_scope.slug)
+        required_scopes = build_required_scopes(request.method, request.path)
+
+        try:
+            view_scopes = set(super(TokenHasRouteScope, self).get_scopes(request, view))
+        except ImproperlyConfigured:
+            view_scopes = []
+
+        required_scopes.extend(view_scopes)
         # call the route the scope
+        if len(required_scopes) < 1: 
+            raise ImproperlyConfigured(
+                "TokenHasRouteScope requires the view to define the required_scopes attribute or an existing scope has a (method, route) pair that matches the views uri."
+            )
         return required_scopes
