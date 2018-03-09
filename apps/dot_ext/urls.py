27119187cb48bae5de8c09798233fff94e3ae1dd
David Gage <whytheplatypus@gmail.com>
2018-03-09 08:13:50 -0500

    Fix oauth2_provider naming after update Named routes were broken after updating oauth2_provider to stable version.

--- apps/dot_ext/urls.py
+++ apps/dot_ext/urls.py
@@ -7,13 +7,13 @@ oauth2_provider_urls = ([
     url(r'^applications/(?P<pk>\d+)/update/$', views.ApplicationUpdate.as_view(), name="update"),
     url(r'^authorize/$', views.AuthorizationView.as_view(), name="authorize"),
     url(r'^scope-authorize/$', views.ScopeAuthorizationView.as_view(), name="scope_authorize"),
-    url(r'', include('oauth2_provider.urls')),
-], 'oauth2_provider', 'oauth2_provider')
+])
 
 router = DefaultRouter()
 router.register(r'tokens', views.AuthorizedTokens, base_name='test')
 
 urlpatterns = [
     url(r'', include(oauth2_provider_urls)),
-    url(r'', include(router.urls))
+    url(r'', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'', include(router.urls, namespace='oauth2_provider'))
 ]
